8a968bd9f3b2623577163c1692346aec4737bc10
HTXuom <xuom.ho@sapotacorp.vn>
2024-06-24 14:15:30 +0700

    commit file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import Link from "next/link"
 export default function Home() {
-    return <h1>welcome Home</h1>
+    return (<><h1>welcome Home!</h1>
+        <Link href="/blog">Blog</Link>
+        <Link href="/products">Products</Link>
+    </>
+    );
 }
--- src/app/products/[productId]/reviews/page.tsx
+++ src/app/products/[productId]/reviews/page.tsx
@@ -1,8 +1,22 @@
 
-export default function ProductDetail({ params, }: {
+import { metadata } from "@/src/layout";
+import { Metadata } from "next";
+type Props = {
     params: {
-        productId: string;
+        productId: string
     };
-    }) {
-    return <h1>Details about product{params.productId}</h1>;
+};
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve('iPhone ${params.productId}')
+        }, 100)
+    });
+    return {
+        title: 'Product ${title}',
+    };
+};
+
+export default function ProductDetail({ params }: Props){
+  return <h1>Details about product{params.productId}</h1>;
 }
